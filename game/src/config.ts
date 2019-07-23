407ff7f5b5dc5d533dc786d16b97b0a6a5cee9af
Jimison-TW <ex3067878@gmail.com>
2019-07-23 19:04:39 +0800

    first commit

---
+++ game/src/config.ts
@@ -0,0 +1,26 @@
+import { Bootloader } from './Bootloader';
+import { MainScene } from './scenes/MainScene';
+
+export const CONFIG: any = {
+    title: 'Free Cell',
+    version: '0.0.1',
+    type: Phaser.AUTO,
+    backgroundColor: '#22a6b3',
+    scale: {
+        parent: 'phaser_container',
+        width: 1920,
+        height: 1080,
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    dom: {
+        createContainer: true
+    },
+    render: {
+        pixelArt: false,
+    },
+    scene: [
+        Bootloader,
+        MainScene
+    ]
+};
